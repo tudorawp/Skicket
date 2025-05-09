862ce797443b8ed08eb7a0893a6e7256e9c952d3
Tudor <oofm752@gmail.com>
2025-05-09 22:10:01 +0300

    heil hitler

---
+++ app/src/main/java/com/example/skicket/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.skicket
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // leagă XML-ul
+    }
+}
